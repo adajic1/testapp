e52ce21b27fca7f6deb256cd56d86f0b81858ec3
adajic1 <adajic1@etf.unsa.ba>
2018-01-24 05:46:35 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 
-/*
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -11,4 +10,3 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-*/
