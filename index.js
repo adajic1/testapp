702d169e36a7353b7acbb08e8fee7ced6bf003f6
adajic1 <adajic1@etf.unsa.ba>
2018-01-24 07:21:19 +0100

    .

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var mysql = require('mysql');
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
 
 var con = mysql.createConnection({
   host     : 'sql11.freemysqlhosting.net',
@@ -11,4 +18,8 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
